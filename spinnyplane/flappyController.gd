54ce311d6296fc700813203365b6ad02b8682582
liamCtrl <122392454+liamCtrl@users.noreply.github.com>
2024-09-30 22:35:29 -0400

    updated files

--- spinnyplane/flappyController.gd
+++ spinnyplane/flappyController.gd
@@ -1,23 +1,41 @@
 extends Node2D
 
 @export var rock_resource : PackedScene 
+@export var plane_resource : PackedScene
 @onready var rock_timer = $rock_timer
+@onready var gameover_control = $GameOver
+@onready var animation_player = $GameOver/AnimationPlayer
+@onready var plane_hit_sound = $sfx/thud
 
-func plane_hit() -> void:
+
+func _ready() -> void:
 	pass
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	rock_timer.start()
+func _process(delta: float) -> void:
+	pass
+
+func plane_hit():
+	gameover_control.visible = true
+	animation_player.play("slide in")
+	plane_hit_sound.play()
+	rock_timer.stop()
 
 func _on_rock_timer_timeout() -> void:
-	if rock_resource :
-		var newRock = rock_resource.instantiate()
-		add_child(newRock)
-		if randf() < 0.5:
-			newRock.position = Vector2(randf_range(0, get_viewport().size.x), 0) 
-		else:
-			newRock.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y)
-		newRock.scale = Vector2(1, 1)
-	else :
-		print("error: no rock resource assigned")
+	var newRock = rock_resource.instantiate()
+	var screen_size = get_viewport_rect().size
+	var random_x = randf_range(0, screen_size.x + 50)
+
+	if randf() < 0.5:
+		newRock.position = Vector2(random_x, 50)
+		newRock.rotation = PI
+	else:
+		newRock.position = Vector2(random_x, screen_size.y - 50)
+	add_child(newRock)
+
+
+func _on_button_pressed() -> void:
+	rock_timer.start()
+	var newPlane = plane_resource.instantiate()
+	add_child(newPlane)
+	newPlane.position = Vector2(83, 319)
+	gameover_control.visible = false
--- spinnyplane/plane_controls.gd
+++ spinnyplane/plane_controls.gd
@@ -2,8 +2,13 @@ extends RigidBody2D
 
 @onready var plane_sprite = $plane_animated_sprite
 
+func _ready() -> void:
+	var velocity = Vector2(200, 0) 
+	linear_velocity = velocity
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("flap") :
+		print("flap")
 		apply_force(Vector2(0, -25000), Vector2.ZERO)
 		plane_sprite.play("flap")
 
